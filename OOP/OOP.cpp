b8304a23b9d4159e4fea050c7c1891578666b806
Даниил Богданов <danya.bogdanov.03@mail.ru>
2023-04-21 23:28:11 +0300

    Добавьте файлы проекта.

---
+++ OOP/OOP.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Location
+{
+protected:
+	int X;//Координата X
+	int Y;//Координата Y
+
+public:
+	//Возвращает X
+	int Get_X()
+	{
+		return X;
+	};
+
+	//Возвращает Y
+	int Get_Y()
+	{
+		return Y;
+	}
+};
+
+int main()
+{
+
+
+
+	return 0;
+}
